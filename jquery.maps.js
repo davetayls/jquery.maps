f564faa29691d1a96a9e3e1c9a7d8bac950ac2bb
Dave Taylor <dave@the-taylors.org>
2011-04-28 10:07:24 +0100

    added access to $holder

--- jquery.maps.js
+++ jquery.maps.js
@@ -1,34 +1,13 @@
-/*
-* jQuery Maps v1 - http://the-taylors.org
-*
-* Open source under the BSD License. 
-* 
-* Copyright © 2011 Dave Taylor http://the-taylors.org
-* All rights reserved.
-* 
-* Redistribution and use in source and binary forms, with or without modification, 
-* are permitted provided that the following conditions are met:
-* 
-* Redistributions of source code must retain the above copyright notice, this list of 
-* conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above copyright notice, this list 
-* of conditions and the following disclaimer in the documentation and/or other materials 
-* provided with the distribution.
-* 
-* Neither the name of the author nor the names of contributors may be used to endorse 
-* or promote products derived from this software without specific prior written permission.
-* 
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
-* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-*  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-*  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
-* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-*  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
-* OF THE POSSIBILITY OF SUCH DAMAGE. 
-*
-*/
+/**
+ * jQuery Maps v0.1 - http://the-taylors.org/jquery.maps
+ * Requires jQuery 1.4.2
+ *
+ * Created by Dave Taylor http://the-taylors.org
+ *
+ * Copyright Software Freedom Conservancy, Inc.
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
+ */
 /*globalconsole window google */
 /*jslint forin:true onevar:false strict:true */
 (function ($) {
@@ -105,6 +84,9 @@
                     if (pinDetails.pin.showInfoWindow) {
                         pinDetails.pin.showInfoWindow();
                     }
+                    if (typeof map.settings.pinCentered === 'function'){
+                        map.settings.pinCentered.call(map, pinDetails);
+                    }
                     $.maps.pinCentered(map, pinDetails);
                     return false;
                 }
@@ -130,7 +112,7 @@
 
             var mapKey = self.attr('id') || 'mapinstance-' + (Math.floor((Math.random() * 1000)) + 100),
                 pins = {};
-            maps[mapKey] = { 'map': map, 'pins': pins, 'settings': settings };
+            maps[mapKey] =  { 'map': map, 'pins': pins, '$holder': self, 'settings': settings };
 
             if (mapPins$.length > 0) {
                 // get pins
